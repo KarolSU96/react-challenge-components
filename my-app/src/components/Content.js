22262ea9e1e5a27c7b966e1a8a0d437b4474ecbb
KarolSU96 <>
2023-11-22 14:42:57 +0000

    add working challenge solution

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,7 +8,8 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
 
@@ -16,8 +17,21 @@ export class Content extends Component {
         setTimeout(()=>{
             this.setState({
                 isLoaded: true,
+                posts : savedPosts
             })
         }, 2000)
+    }
+
+    handleChange = (event) => {
+        const name = event.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter(post => {
+            return post.name.toLowerCase().includes(name)
+        })
+
+        this.setState({
+            posts: filteredPosts
+        })
+
     }
     
     render() {
@@ -26,12 +40,21 @@ export class Content extends Component {
                 
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form htmlFor='searchInput'>
+                        <label>Search: </label>
+                        <input
+                        id ='searchinput'
+                        type='search'
+                        onChange={(event) => this.handleChange(event)}
+                    />
+                    <h4>posts found {this.state.posts.length}</h4>
+                    </form>
                 </div>
 
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ?
-                        <PostItem savedPosts={savedPosts} />
+                        <PostItem savedPosts={this.state.posts} />
                         : <Loader />
                     }
                 </div>
